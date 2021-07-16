f14fd66994ac9e5a5f7a0ab7f734bc59031084ce
suhobski <suhobski@mail.ru>
2021-07-16 06:45:45 +0300

    refactor: clear code

--- src/store/actions/catalog.js
+++ src/store/actions/catalog.js
@@ -7,24 +7,15 @@ export function fetchCatalog() {
     dispatch(fetchCatalogStart())
     try {
       const cachedData = window.localStorage.getItem('exercises');
-    
       if(cachedData) {
         dispatch(fetchCatalogSuccess(JSON.parse(cachedData)['-MdsKrLvEFluhJjSGPQq']))
         return
       }
 
       const response = await axios.get('https://football-exercies-default-rtdb.europe-west1.firebasedatabase.app/exercises.json');
-
-      console.log('response', response);
-
-      const catalog = []
       const data = await response.data 
-      Object.keys(data).forEach(key => {
-        catalog.push(data[key])
-      })
 
       window.localStorage.setItem('exercises', JSON.stringify(data));
-      console.log();
       dispatch(fetchCatalogSuccess(data['-MdsKrLvEFluhJjSGPQq']))
       return
     } catch (e) {
